de6f526bfaacab465369edd0864b89ef59ce66e4
menyuhan <hanyumen2009@163.com>
2020-06-26 23:26:11 +0800

    Update .cproject

--- G2553/P02 TimerA Blink LED1/main.c
+++ G2553/P02 TimerA Blink LED1/main.c
@@ -1,14 +1,14 @@
 #include <msp430g2553.h> 
-#include"Clock.h"
-#include"GPIO.h"
-#include"TimerA.h"
+#include "Clock.h"
+#include "GPIO.h"
+#include "TimerA.h"
 
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
 	BCS_Init(16, 16, 0, 0, 1, 0);
 	TA0_Init('A', 1, 1, 12000);
 	GPIO_Init();
-	_EINT();					//开总中断
+	_EINT(); 					//开总中断
 	while (1);
 }
